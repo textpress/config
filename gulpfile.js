197ed4713fdbad9757cbe6938a7ddbc90a540260
Alexander Golovanov <agolovanov@textpress.co>
2017-11-29 21:03:43 -0600

    Misc: Add documentation generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,3 @@
 "use strict";
-const gulp = require( "gulp" );
-const babel = require( "gulp-babel" );
-const clean = require( "gulp-clean" );
-const sourcemaps = require( "gulp-sourcemaps" );
-
-const buildFolder = "lib";
 
 require( "@textpress/gulp-bump-version" ).registerTask();
-
-gulp.task( "build", [ "copy", "babel" ] );
-
-gulp.task( "clean", () => {
-    return gulp.src( [ buildFolder ], { read: false } )
-        .pipe( clean() );
-} );
-
-gulp.task( "copy", [ "clean" ], () => {
-    return gulp.src( [ "src/**/*", "!**/*.js", "!**/__tests__", "!**/__tests__/**" ] )
-        .pipe( gulp.dest( buildFolder ) );
-} );
-
-gulp.task( "babel", [ "clean" ], () => {
-    return gulp.src( [ "src/**/*.js", "!**/__tests__", "!**/__tests__/**" ] )
-        .pipe( sourcemaps.init() )
-        .pipe( babel() )
-        .pipe( sourcemaps.write( "." ) )
-        .pipe( gulp.dest( buildFolder ) );
-} );
